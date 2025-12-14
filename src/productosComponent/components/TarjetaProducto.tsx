5c3828eb65bd5d35889eb6d45c4ea311d446da5c
Ricardo Díaz <ric.diazs@duocuc.cl>
2025-12-14 17:12:44 -0300

    vista 'productos' consume datos de API
    Ahora la vista de 'Productos' obtiene los datos desde microservicio
    'registro-productos' para mostrar catalogo.

--- src/productosComponent/components/TarjetaProducto.tsx
+++ src/productosComponent/components/TarjetaProducto.tsx
@@ -1,36 +1,38 @@
 import { useNavigate } from "react-router-dom";
-import type { PeliculaType } from "../datosProductos";
+import type { Producto } from "../../registroProductoComponents/Producto";
 
 interface TarjetaProductoProps {
     agregarProd: (idProd: number) => void;
-    producto: PeliculaType;
+    producto: Producto;
 }
 
 export const TarjetaProducto = ({agregarProd, producto}:TarjetaProductoProps) => {
     const navigate = useNavigate();
 
-    const navegarDetallePelicula = (pelicula: PeliculaType) => {
+    const navegarDetallePelicula = (pelicula: Producto) => {
         navigate(`/detalle-pelicula/${pelicula.id}`)
     }
+
+    const imagenUrl = producto.imgUrl;
 
     return(
         <div className="col-12 col-md-3 mb-4">
             <div className="card w-100 h-100">
                 <img
-                    src={`${producto.imagenSrc}`}
+                    src={`http://localhost:8081${imagenUrl}`}
                     className="card-img-top cursor-pointer object-fit-cover"
-                    alt={`${producto.imagenAlt}`}
+                    alt={`Poster de pelicula ${producto.nombreProducto}`}
                     onClick={() => navegarDetallePelicula(producto)}
                     style={{cursor: 'pointer'}}
                 />
                 <div className="card-body">
                     <h5 className="card-title">
                         <a onClick = {() => {navegarDetallePelicula(producto)}} style={{cursor: 'pointer', color: '#042dd1'}}>
-                            {producto.titulo}
+                            {producto.nombreProducto}
                         </a>
                     </h5>
                     <p className="card-text">{`Precio: $${producto.precio}`}</p>
-                    <button className="btn btn-primary" onClick={() => agregarProd(producto.id)}>Añadir</button>
+                    <button className="btn btn-primary" onClick={() => agregarProd(producto.id!)}>Añadir</button>
                 </div>
             </div>
         </div>
